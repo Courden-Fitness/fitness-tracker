a43f4c715f363c16adbc070947ab05296a138487
Hayden Vick <vickhayden@gmail.com>
2023-05-09 13:38:11 -0500

    frontend setup w/Courtney

---
+++ client/src/components/Activities.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Activities = () => {
+    return (
+        <>
+        <h1>Welcome from Activities!</h1>
+        </>
+    )
+}
+
+export default Activities;
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <>
+        <h1>Welcome from Login!</h1>
+        </>
+    )
+}
+
+export default Login;
